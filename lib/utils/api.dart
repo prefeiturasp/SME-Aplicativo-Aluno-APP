ca186a765dda51d4fb48c727c1561b13b15f8bcf
Diego Collares <diego.collares@gmail.com>
2020-04-05 18:03:23 -0300

    declared andpoint api

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,3 +1,6 @@
 class Api {
-  static String BASE_URL = "https://mockap.com";
+  static final String hostDev = 'https://demo9546116.mockable.io/api/v1';
+  static final String hostHml = '#';
+  static final String hostPrd = '#';
+  static final String host = hostDev;
 }
