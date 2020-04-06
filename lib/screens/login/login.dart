af8355ebfc99fcb3bbec9a6030fea2ad35b5dd99
Diego Collares <diego.collares@gmail.com>
2020-04-06 01:14:48 -0300

    validate input data

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:brasil_fields/brasil_fields.dart';
+import 'package:cpf_cnpj_validator/cpf_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -10,144 +11,218 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _cpfController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   bool _showPassword = true;
+  String _cpf = '';
+  String _dataNnascimentoAluno = '';
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var screenHeight = (size.height - MediaQuery.of(context).padding.top) / 100;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.all(screenHeight * 2.5),
           child: Column(
             children: <Widget>[
               Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 60, bottom: 60),
-                width: double.infinity,
-                child: Image.asset("assets/images/logo_app.png"),
-              ),
-              Form(
                 child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.only(left: 10),
-                        decoration: BoxDecoration(
-                          color: Color(0xfff0f0f0),
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: Color(0xff8b8b8b), width: 2.0)),
-                        ),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Usuário',
-                            labelStyle: TextStyle(color: Color(0xff8e8e8e)),
-                            // hintText: "Digite o CPF do responsável",
-                            border: InputBorder.none,
-                          ),
-                          inputFormatters: [
-                            WhitelistingTextInputFormatter.digitsOnly,
-                            CpfInputFormatter(),
-                          ],
-                          keyboardType: TextInputType.number,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      AutoSizeText(
-                        "Digite o CPF do resposável",
-                        maxFontSize: 14,
-                        minFontSize: 12,
-                        style: TextStyle(color: Color(0xff979797)),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 40),
-                        padding: EdgeInsets.only(left: 10),
-                        decoration: BoxDecoration(
-                          color: Color(0xfff0f0f0),
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: Color(0xff8b8b8b), width: 2.0)),
-                        ),
-                        child: TextFormField(
-                          obscureText: _showPassword,
-                          decoration: InputDecoration(
-                            suffixIcon: IconButton(
-                              icon: _showPassword
-                                  ? Icon(FontAwesomeIcons.eye)
-                                  : Icon(FontAwesomeIcons.eyeSlash),
-                              color: Color(0xff6e6e6e),
-                              iconSize: 18,
-                              onPressed: () {
-                                setState(() {
-                                  _showPassword = !_showPassword;
-                                });
-                              },
-                            ),
-                            labelText: 'Senha',
-                            labelStyle: TextStyle(color: Color(0xff8e8e8e)),
-                            // hintText: "Data de nascimento do aluno",
-                            border: InputBorder.none,
-                          ),
-                          inputFormatters: [
-                            WhitelistingTextInputFormatter.digitsOnly,
-                            DataInputFormatter(),
-                          ],
-                          keyboardType: TextInputType.number,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      AutoSizeText(
-                        "Digite a data de nacimento do aluno dd/mm/aaaa",
-                        maxFontSize: 14,
-                        minFontSize: 12,
-                        style: TextStyle(
-                          color: Color(0xff979797),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 50.0,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Color(0xffd06d12),
-                            borderRadius: BorderRadius.circular(25)),
-                        child: FlatButton(
-                          onPressed: () {},
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                "ENTRAR",
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(
+                          top: screenHeight * 8, bottom: screenHeight * 6),
+                      width: double.infinity,
+                      child: Image.asset("assets/images/logo_app.png"),
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.only(left: screenHeight * 2),
+                              decoration: BoxDecoration(
+                                color: Color(0xfff0f0f0),
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color: _cpf.isEmpty
+                                            ? Color(0xff8b8b8b)
+                                            : Color(0xffD06D12),
+                                        width: screenHeight * 0.39)),
+                              ),
+                              child: TextFormField(
+                                controller: _cpfController,
                                 style: TextStyle(
-                                    fontSize: 16.0,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700),
+                                    color: Color(0xff333333),
+                                    fontWeight: FontWeight.w600),
+                                decoration: InputDecoration(
+                                  labelText: 'Usuário',
+                                  labelStyle:
+                                      TextStyle(color: Color(0xff8e8e8e)),
+                                  errorStyle:
+                                      TextStyle(fontWeight: FontWeight.w700),
+                                  border: InputBorder.none,
+                                ),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _cpf = _cpfController.text;
+                                  });
+                                  _formKey.currentState.validate();
+                                },
+                                validator: (_) {
+                                  if (_cpf.isEmpty) {
+                                    return 'Campo obrigatório, não pode ficar em branco.';
+                                  }
+
+                                  if (_cpf.length == 14 &&
+                                      !CPFValidator.isValid(_cpf)) {
+                                    return 'CPF inválido';
+                                  }
+
+                                  return null;
+                                },
+                                inputFormatters: [
+                                  WhitelistingTextInputFormatter.digitsOnly,
+                                  CpfInputFormatter(),
+                                ],
+                                keyboardType: TextInputType.number,
                               ),
-                              SizedBox(
-                                width: 15.0,
+                            ),
+                            SizedBox(
+                              height: screenHeight * 1,
+                            ),
+                            AutoSizeText(
+                              "Digite o CPF do resposável",
+                              maxFontSize: 14,
+                              minFontSize: 12,
+                              style: TextStyle(color: Color(0xff979797)),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: screenHeight * 5),
+                              padding: EdgeInsets.only(left: screenHeight * 2),
+                              decoration: BoxDecoration(
+                                color: Color(0xfff0f0f0),
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color: _dataNnascimentoAluno.isEmpty
+                                            ? Color(0xff8b8b8b)
+                                            : Color(0xffD06D12),
+                                        width: screenHeight * 0.39)),
                               ),
-                              Icon(
-                                FontAwesomeIcons.chevronRight,
-                                color: Color(0xffffd037),
-                                size: 18,
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ]),
+                              child: TextFormField(
+                                controller: _passwordController,
+                                style: TextStyle(
+                                    color: Color(0xff333333),
+                                    fontWeight: FontWeight.w600),
+                                obscureText: _showPassword,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _dataNnascimentoAluno =
+                                        _passwordController.text;
+                                  });
+                                },
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    icon: _showPassword
+                                        ? Icon(FontAwesomeIcons.eye)
+                                        : Icon(FontAwesomeIcons.eyeSlash),
+                                    color: Color(0xff6e6e6e),
+                                    iconSize: screenHeight * 3.0,
+                                    onPressed: () {
+                                      setState(() {
+                                        _showPassword = !_showPassword;
+                                      });
+                                    },
+                                  ),
+                                  labelText: 'Senha',
+                                  labelStyle:
+                                      TextStyle(color: Color(0xff8e8e8e)),
+                                  errorStyle:
+                                      TextStyle(fontWeight: FontWeight.w700),
+                                  // hintText: "Data de nascimento do aluno",
+                                  border: InputBorder.none,
+                                ),
+                                validator: (_) {
+                                  if (_dataNnascimentoAluno.isEmpty) {
+                                    return 'Campo obrigatório, não pode ficar em branco.';
+                                  }
+
+                                  return null;
+                                },
+                                inputFormatters: [
+                                  WhitelistingTextInputFormatter.digitsOnly,
+                                  DataInputFormatter(),
+                                ],
+                                keyboardType: TextInputType.number,
+                              ),
+                            ),
+                            SizedBox(
+                              height: screenHeight * 1,
+                            ),
+                            AutoSizeText(
+                              "Digite a data de nacimento do aluno dd/mm/aaaa",
+                              maxFontSize: 14,
+                              minFontSize: 12,
+                              style: TextStyle(
+                                color: Color(0xff979797),
+                              ),
+                            ),
+                            SizedBox(
+                              height: screenHeight * 7,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: screenHeight * 7,
+                              decoration: BoxDecoration(
+                                  color: Color(0xffd06d12),
+                                  borderRadius: BorderRadius.circular(
+                                      screenHeight * 3.5)),
+                              child: FlatButton(
+                                onPressed: () {
+                                  if (_formKey.currentState.validate()) {
+                                    // Scaffold.of(context).showSnackBar(SnackBar(
+                                    //     content: Text('Processing Data')));
+                                  }
+                                },
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    AutoSizeText(
+                                      "ENTRAR",
+                                      maxFontSize: 16,
+                                      minFontSize: 14,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                    SizedBox(
+                                      width: screenHeight * 3,
+                                    ),
+                                    Icon(
+                                      FontAwesomeIcons.chevronRight,
+                                      color: Color(0xffffd037),
+                                      size: screenHeight * 3,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ]),
+                    ),
+                  ],
+                ),
               ),
               Container(
-                height: 40,
+                height: screenHeight * 6,
                 margin: EdgeInsets.only(top: 70),
                 child: Image.asset("assets/images/logo_sme.png",
                     fit: BoxFit.cover),
