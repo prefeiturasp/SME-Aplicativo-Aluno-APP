676cf3acb1d5f7c514b19b5fb934d2cba19cff7f
Diego Collares <diego.collares@gmail.com>
2020-04-06 01:13:35 -0300

    defined api host

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,6 +1,7 @@
 class Api {
-  static final String hostDev = 'https://demo9546116.mockable.io/api/v1';
-  static final String hostHml = '#';
-  static final String hostPrd = '#';
-  static final String host = hostDev;
+  static const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
+  static final String HOST_DEV = 'https://demo9546116.mockable.io/api/v1';
+  static final String HOST_HML = '#';
+  static final String HOST_PROD = '#';
+  static final String HOST = HOST_DEV;
 }
