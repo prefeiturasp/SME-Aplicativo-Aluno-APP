127fcec0a5eae114f6400454e8effd3fa90ecc05
Diego Collares <diego.collares@gmail.com>
2020-04-07 14:56:53 -0300

    created component

---
+++ lib/widgets/tag/tag_custom.dart
@@ -0,0 +1,37 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class TagCustom extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  TagCustom({
+    @required this.text,
+    @required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var screenHeight = (size.height - MediaQuery.of(context).padding.top) / 100;
+    return Container(
+      padding: EdgeInsets.only(
+          left: screenHeight * 2.2,
+          top: screenHeight * 0.9,
+          right: screenHeight * 2.2,
+          bottom: screenHeight * 0.9),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(
+          Radius.circular(screenHeight * 2),
+        ),
+      ),
+      child: AutoSizeText(
+        text,
+        maxFontSize: 12,
+        minFontSize: 10,
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+      ),
+    );
+  }
+}
