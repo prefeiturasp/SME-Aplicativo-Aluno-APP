55983af1bb223d4d04c8e510587f127801fb4162
Diego Collares <diego.collares@gmail.com>
2020-04-16 06:09:12 -0300

    updated list screen

--- lib/screens/students/list_studants.dart
+++ lib/screens/students/list_studants.dart
@@ -5,9 +5,11 @@ import 'package:getflutter/components/loader/gf_loader.dart';
 import 'package:getflutter/size/gf_size.dart';
 import 'package:getflutter/types/gf_loader_type.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sme_app_aluno/controllers/students.controller.dart';
 import 'package:sme_app_aluno/models/student.dart';
 import 'package:sme_app_aluno/screens/dashboard/dashboard.dart';
+import 'package:sme_app_aluno/screens/login/login.dart';
 import 'package:sme_app_aluno/screens/students/widgets/cards/card_students.dart';
 import 'package:sme_app_aluno/screens/widgets/tag/tag_custom.dart';
 
@@ -37,6 +39,38 @@ class ListStudants extends StatelessWidget {
     return new Column(children: list);
   }
 
+  removeValuesStorage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
+  Future<bool> _onBackPress(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Atenção"),
+            content: Text("Deseja sair do aplicativo?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("SIM"),
+                onPressed: () {
+                  removeValuesStorage();
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Login()));
+                },
+              ),
+              FlatButton(
+                child: Text("NÃO"),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -46,7 +80,6 @@ class ListStudants extends StatelessWidget {
         Provider.of<StudentsController>(context, listen: false);
 
     studentsController.loadingStudents(cpf, token);
-
     return Scaffold(
       backgroundColor: Color(0xffE5E5E5),
       appBar: AppBar(
@@ -54,58 +87,61 @@ class ListStudants extends StatelessWidget {
         backgroundColor: Color(0xffEEC25E),
         automaticallyImplyLeading: false,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(screenHeight * 2.5),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: screenHeight * 2.5,
-              ),
-              AutoSizeText(
-                "ALUNOS CADASTRADOS PARA O RESPONSÁVEL",
-                maxFontSize: 16,
-                minFontSize: 14,
-                style: TextStyle(
-                    color: Color(0xffDE9524), fontWeight: FontWeight.w500),
-              ),
-              SizedBox(
-                height: screenHeight * 3.5,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: screenHeight * 74,
-                child: Observer(builder: (context) {
-                  if (studentsController.isLoading) {
-                    return GFLoader(
-                      type: GFLoaderType.square,
-                      loaderColorOne: Color(0xffDE9524),
-                      loaderColorTwo: Color(0xffC65D00),
-                      loaderColorThree: Color(0xffC65D00),
-                      size: GFSize.LARGE,
-                    );
-                  } else {
-                    return ListView.builder(
-                      itemCount: studentsController.listStudents.length,
-                      itemBuilder: (context, index) {
-                        final dados = studentsController.listStudents;
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            TagCustom(
-                                text: "${dados[index].descricaoTipoEscola}",
-                                color: Color(0xffC65D00)),
-                            _listStudents(dados[index].student, context),
-                          ],
-                        );
-                      },
-                    );
-                  }
-                }),
-              )
-            ],
+      body: WillPopScope(
+        onWillPop: () => _onBackPress(context),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(screenHeight * 2.5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: screenHeight * 2.5,
+                ),
+                AutoSizeText(
+                  "ALUNOS CADASTRADOS PARA O RESPONSÁVEL",
+                  maxFontSize: 16,
+                  minFontSize: 14,
+                  style: TextStyle(
+                      color: Color(0xffDE9524), fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: screenHeight * 3.5,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: screenHeight * 74,
+                  child: Observer(builder: (context) {
+                    if (studentsController.isLoading) {
+                      return GFLoader(
+                        type: GFLoaderType.square,
+                        loaderColorOne: Color(0xffDE9524),
+                        loaderColorTwo: Color(0xffC65D00),
+                        loaderColorThree: Color(0xffC65D00),
+                        size: GFSize.LARGE,
+                      );
+                    } else {
+                      return ListView.builder(
+                        itemCount: studentsController.listStudents.length,
+                        itemBuilder: (context, index) {
+                          final dados = studentsController.listStudents;
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              TagCustom(
+                                  text: "${dados[index].descricaoTipoEscola}",
+                                  color: Color(0xffC65D00)),
+                              _listStudents(dados[index].student, context),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  }),
+                )
+              ],
+            ),
           ),
         ),
       ),
