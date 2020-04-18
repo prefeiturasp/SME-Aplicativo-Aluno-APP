b6dd64f190600beb0c52305cec44236555a17720
Diego Collares <diego.collares@gmail.com>
2020-04-18 00:07:31 -0300

    created storage util

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -1,6 +1,11 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Storage {
+  void insertString(String key, String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+  }
+
   void removeAllValues() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.clear();
