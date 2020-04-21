ec3b19f088f8547858c2592dc71be498acd723db
Diego Collares <diego.collares@gmail.com>
2020-04-21 00:09:32 -0300

    updated contract interface with model

--- lib/interfaces/student_repository_interface.dart
+++ lib/interfaces/student_repository_interface.dart
@@ -1,5 +1,5 @@
-import 'package:sme_app_aluno/models/student.dart';
+import 'package:sme_app_aluno/models/student/data_student.dart';
 
 abstract class IStudentsRepository {
-  Future<List<Student>> fetchStudents(String cpf, String token);
+  Future<DataStudent> fetchStudents(String cpf, String token);
 }
