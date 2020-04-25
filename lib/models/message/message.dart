239816e8af2578d91f382f951dac29cd2d834118
Diego Collares <diego.collares@gmail.com>
2020-04-25 15:14:30 -0300

    updated Message model

--- lib/models/message/message.dart
+++ lib/models/message/message.dart
@@ -3,54 +3,50 @@ class Message {
   String mensagem;
   String titulo;
   String grupo;
-  DateTime dataEnvio;
-  DateTime dataExpiracao;
-  DateTime criadoEm;
+  String dataEnvio;
+  String dataExpiracao;
+  String criadoEm;
   String criadoPor;
-  DateTime alteradoEm;
+  String alteradoEm;
   String alteradoPor;
 
-  Message({
-    this.id, 
-    this.mensagem,
-    this.titulo,
-    this.grupo,
-    this.dataEnvio,
-    this.dataExpiracao,
-    this.criadoEm,
-    this.criadoPor,
-    this.alteradoEm,
-    this.alteradoPor
-  });
+  Message(
+      {this.id,
+      this.mensagem,
+      this.titulo,
+      this.grupo,
+      this.dataEnvio,
+      this.dataExpiracao,
+      this.criadoEm,
+      this.criadoPor,
+      this.alteradoEm,
+      this.alteradoPor});
 
   Message.fromJson(Map<String, dynamic> json) {
-    id = json["id"];
-    mensagem = json["mensagem"];
-    titulo = json["titulo"];
-    grupo = json["grupo"];
-    dataEnvio = DateTime.tryParse(json["dataEnvio"]);
-    dataExpiracao = DateTime.tryParse(json["dataExpiracao"]);
-    criadoEm = DateTime.tryParse(json["criadoEm"]);
-    criadoPor = json["criadoPor"];
-    alteradoEm = json["alteradoEm"] != null ? DateTime.tryParse(json["alteradoEm"]) : null;
-    alteradoPor = json["alteradoPor"];
+    id = json['id'];
+    mensagem = json['mensagem'];
+    titulo = json['titulo'];
+    grupo = json['grupo'];
+    dataEnvio = json['dataEnvio'];
+    dataExpiracao = json['dataExpiracao'];
+    criadoEm = json['criadoEm'];
+    criadoPor = json['criadoPor'];
+    alteradoEm = json['alteradoEm'];
+    alteradoPor = json['alteradoPor'];
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> json = new Map<String, dynamic>();
-    json["id"] = id;
-    json["mensagem"] = mensagem;
-    json["titulo"] = titulo;
-    json["grupo"] = grupo;
-    json["dataEnvio"] = dataEnvio;
-    json["dataExpiracao"] = dataExpiracao;
-    json["criadoEm"] = criadoEm;
-    json["criadoPor"] = criadoPor;
-    json["alteradoEm"] = alteradoEm;
-    json["alteradoPor"] = alteradoPor;
-    return json;
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['mensagem'] = this.mensagem;
+    data['titulo'] = this.titulo;
+    data['grupo'] = this.grupo;
+    data['dataEnvio'] = this.dataEnvio;
+    data['dataExpiracao'] = this.dataExpiracao;
+    data['criadoEm'] = this.criadoEm;
+    data['criadoPor'] = this.criadoPor;
+    data['alteradoEm'] = this.alteradoEm;
+    data['alteradoPor'] = this.alteradoPor;
+    return data;
   }
-
-  static List<Message> fromJsonList(List<Map<String, dynamic>> json) => 
-    json.map<Message>((i) => Message.fromJson(i)).toList();
 }
