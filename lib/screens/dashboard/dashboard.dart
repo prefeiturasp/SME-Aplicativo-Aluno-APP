2e65881411aeb39c8ff6bba1eae43146c5ad402f
Diego Collares <diego.collares@gmail.com>
2020-05-04 23:07:07 -0300

    updated layout card recent message

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -4,11 +4,13 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:getflutter/components/loader/gf_loader.dart';
 import 'package:getflutter/size/gf_size.dart';
 import 'package:getflutter/types/gf_loader_type.dart';
+import 'package:provider/provider.dart';
 import 'package:sme_app_aluno/controllers/messages.controller.dart';
 import 'package:sme_app_aluno/models/student/student.dart';
 import 'package:sme_app_aluno/screens/widgets/cards/index.dart';
 import 'package:sme_app_aluno/screens/drawer_menu/drawer_menu.dart';
 import 'package:sme_app_aluno/screens/widgets/tag/tag_custom.dart';
+import 'package:sme_app_aluno/services/connectivity_service.dart';
 
 class Dashboard extends StatefulWidget {
   final Student student;
@@ -38,6 +40,12 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     var screenHeight = (size.height - MediaQuery.of(context).padding.top) / 100;
+    var connectivityService = Provider.of<ConnectivityService>(context);
+
+    // print("connectionStatus: -----------------");
+    // print("${connectivityService.connectionStatusController}");
+    // print("connectionStatus: -----------------");
+
     return Scaffold(
       backgroundColor: Color(0xffE5E5E5),
       appBar: AppBar(
@@ -48,71 +56,62 @@ class _DashboardState extends State<Dashboard> {
         child: Container(
           width: MediaQuery.of(context).size.width,
           padding: EdgeInsets.all(screenHeight * 2.5),
-          child: Observer(builder: (context) {
-            var recentMessage = _messagesController.messages;
-            if (recentMessage != null) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  SizedBox(
-                    height: screenHeight * 2.5,
-                  ),
-                  TagCustom(
-                      text: widget.groupSchool ?? "Não informado",
-                      color: Color(0xffEEC25E),
-                      textColor: Color(0xffD06D12)),
-                  CardResumeStudent(
-                    student: widget.student,
-                  ),
-                  Visibility(
-                      child: CardRecentMessage(
-                        message: recentMessage.first,
-                        countMessages: recentMessage.length,
-                        token: widget.token,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              SizedBox(
+                height: screenHeight * 2.5,
+              ),
+              TagCustom(
+                  text: widget.groupSchool ?? "Não informado",
+                  color: Color(0xffEEC25E),
+                  textColor: Color(0xffD06D12)),
+              CardResumeStudent(
+                student: widget.student,
+              ),
+              Observer(builder: (context) {
+                if (_messagesController.messages != null &&
+                    _messagesController.messages.isNotEmpty) {
+                  return Visibility(
+                      visible: _messagesController.isLoading,
+                      child: GFLoader(
+                        type: GFLoaderType.square,
+                        loaderColorOne: Color(0xffDE9524),
+                        loaderColorTwo: Color(0xffC65D00),
+                        loaderColorThree: Color(0xffC65D00),
+                        size: GFSize.LARGE,
                       ),
-                      visible: _messagesController.isLoading == null ||
-                          _messagesController.isLoading == false,
-                      replacement: Container(
-                        child: GFLoader(
-                          type: GFLoaderType.square,
-                          loaderColorOne: Color(0xffDE9524),
-                          loaderColorTwo: Color(0xffC65D00),
-                          loaderColorThree: Color(0xffC65D00),
-                          size: GFSize.LARGE,
-                        ),
-                      )),
-                  CardAlert(
-                    title: "ALERTA DE NOTAS",
-                    icon: Icon(
-                      FontAwesomeIcons.envelopeOpen,
-                      color: Color(0xffFFD037),
-                      size: screenHeight * 6,
-                    ),
-                    text:
-                        "Em breve você visualizará alertas de notas neste espaço. Aguarde as próximas atualizações do aplicativo.",
-                  ),
-                  CardAlert(
-                    title: "ALERTA DE FREQUÊNCIA",
-                    icon: Icon(
-                      FontAwesomeIcons.envelopeOpen,
-                      color: Color(0xffFFD037),
-                      size: screenHeight * 6,
-                    ),
-                    text:
-                        "Em breve você visualizará alertas de frequência neste espaço. Aguarde as próximas atualizações do aplicativo.",
-                  )
-                ],
-              );
-            } else {
-              return GFLoader(
-                type: GFLoaderType.square,
-                loaderColorOne: Color(0xffDE9524),
-                loaderColorTwo: Color(0xffC65D00),
-                loaderColorThree: Color(0xffC65D00),
-                size: GFSize.LARGE,
-              );
-            }
-          }),
+                      replacement: CardRecentMessage(
+                        message: _messagesController.messages.first,
+                        countMessages: _messagesController.messages.length,
+                        token: widget.token,
+                      ));
+                } else {
+                  return CardRecentMessage();
+                }
+              }),
+              CardAlert(
+                title: "ALERTA DE NOTAS",
+                icon: Icon(
+                  FontAwesomeIcons.envelopeOpen,
+                  color: Color(0xffFFD037),
+                  size: screenHeight * 6,
+                ),
+                text:
+                    "Em breve você visualizará alertas de notas neste espaço. Aguarde as próximas atualizações do aplicativo.",
+              ),
+              CardAlert(
+                title: "ALERTA DE FREQUÊNCIA",
+                icon: Icon(
+                  FontAwesomeIcons.envelopeOpen,
+                  color: Color(0xffFFD037),
+                  size: screenHeight * 6,
+                ),
+                text:
+                    "Em breve você visualizará alertas de frequência neste espaço. Aguarde as próximas atualizações do aplicativo.",
+              )
+            ],
+          ),
         ),
       ),
       drawer: DrawerMenu(
