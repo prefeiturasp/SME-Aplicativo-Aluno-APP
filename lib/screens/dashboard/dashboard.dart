1bc154dd893d86c266649e7f6730c611276ce5b9
Diego Collares <diego.collares@gmail.com>
2020-05-04 23:58:25 -0300

    remove import

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -4,7 +4,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:getflutter/components/loader/gf_loader.dart';
 import 'package:getflutter/size/gf_size.dart';
 import 'package:getflutter/types/gf_loader_type.dart';
-import 'package:provider/provider.dart';
 import 'package:sme_app_aluno/controllers/messages.controller.dart';
 import 'package:sme_app_aluno/models/student/student.dart';
 import 'package:sme_app_aluno/screens/widgets/cards/index.dart';
