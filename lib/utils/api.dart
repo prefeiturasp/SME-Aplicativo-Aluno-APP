f77e3b45af948c06180cb53e896d88390c97ec77
Diego Collares <diego.collares@gmail.com>
2020-06-12 13:37:23 -0300

    updated

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -4,5 +4,5 @@ class Api {
   static final String HOST_HML =
       'https://hom-appaluno.sme.prefeitura.sp.gov.br/api/v1';
   static final String HOST_PROD = '#';
-  static final String HOST = HOST_HML;
+  static final String HOST = HOST_DEV;
 }
