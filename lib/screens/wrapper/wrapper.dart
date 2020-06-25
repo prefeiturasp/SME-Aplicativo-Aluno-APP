788eb04c4ebeda3e8fc50bf30caf7c05a9b33000
Diego Collares <diego.collares@gmail.com>
2020-06-24 23:27:15 -0300

    created connection status verify

--- lib/screens/wrapper/wrapper.dart
+++ lib/screens/wrapper/wrapper.dart
@@ -1,13 +1,17 @@
+import 'package:background_fetch/background_fetch.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:getflutter/components/loader/gf_loader.dart';
 import 'package:getflutter/size/gf_size.dart';
 import 'package:getflutter/types/gf_loader_type.dart';
+import 'package:provider/provider.dart';
 import 'package:sme_app_aluno/controllers/authenticate.controller.dart';
 import 'package:sme_app_aluno/models/message/message.dart';
 import 'package:sme_app_aluno/screens/login/login.dart';
 import 'package:sme_app_aluno/screens/messages/view_message_notification.dart';
+import 'package:sme_app_aluno/screens/not_internet/not_internet.dart';
 import 'package:sme_app_aluno/screens/students/list_studants.dart';
+import 'package:sme_app_aluno/utils/conection.dart';
 import 'package:sme_app_aluno/utils/storage.dart';
 
 class Wrapper extends StatefulWidget {
@@ -91,25 +95,34 @@ class _WrapperState extends State<Wrapper> {
 
   @override
   Widget build(BuildContext context) {
-    bool isAuthenticated = _cpf != null && _token != null;
+    var connectionStatus = Provider.of<ConnectivityStatus>(context);
 
-    bool notErrorAuthenticate = _authenticateController.currentUser != null &&
-        _authenticateController.currentUser.erros[0].isNotEmpty;
-
-    if (!isAuthenticated || notErrorAuthenticate) {
-      return Scaffold(
-          backgroundColor: Colors.white,
-          body: _loading
-              ? GFLoader(
-                  type: GFLoaderType.square,
-                  loaderColorOne: Color(0xffDE9524),
-                  loaderColorTwo: Color(0xffC65D00),
-                  loaderColorThree: Color(0xffC65D00),
-                  size: GFSize.LARGE,
-                )
-              : Login());
+    if (connectionStatus == ConnectivityStatus.Offline) {
+      BackgroundFetch.stop().then((int status) {
+        print('[BackgroundFetch] stop success: $status');
+      });
+      return NotInteernet();
     } else {
-      return ListStudants(cpf: _cpf, token: _token, password: _password);
+      bool isAuthenticated = _cpf != null && _token != null;
+
+      bool notErrorAuthenticate = _authenticateController.currentUser != null &&
+          _authenticateController.currentUser.erros[0].isNotEmpty;
+
+      if (!isAuthenticated || notErrorAuthenticate) {
+        return Scaffold(
+            backgroundColor: Colors.white,
+            body: _loading
+                ? GFLoader(
+                    type: GFLoaderType.square,
+                    loaderColorOne: Color(0xffDE9524),
+                    loaderColorTwo: Color(0xffC65D00),
+                    loaderColorThree: Color(0xffC65D00),
+                    size: GFSize.LARGE,
+                  )
+                : Login());
+      } else {
+        return ListStudants(cpf: _cpf, token: _token, password: _password);
+      }
     }
   }
 }
