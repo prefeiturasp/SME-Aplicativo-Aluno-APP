5b9ca38394e7d2a9fcc0430e497be7d3cb28377a
Diego Collares <diego.collares@gmail.com>
2020-06-27 00:05:31 -0300

    updated inteerface

--- lib/interfaces/first_access_repository_interface.dart
+++ lib/interfaces/first_access_repository_interface.dart
@@ -1,5 +1,9 @@
-import 'package:sme_app_aluno/models/user/data.dart';
+import 'package:sme_app_aluno/models/first_access/data.dart';
 
 abstract class IFirstAccessRepository {
   Future<Data> changeNewPassword(int id, String password);
+
+  Future<Data> changeEmail(int id, String email);
+
+  Future<Data> changePhone(int id, String phone);
 }
