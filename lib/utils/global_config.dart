06367c21305ee617caaa889127c5af41005a3c09
Diego Collares <diego.collares@gmail.com>
2020-06-29 23:30:36 -0300

    updated SENTRY_DSN

--- lib/utils/global_config.dart
+++ lib/utils/global_config.dart
@@ -1,4 +1,4 @@
 class GlobalConfig {
   static String Environment = "dev";
-  static String SENTRY_DSN = "ae61dd884a8c4e78800817bcff385afc";
+  static String SENTRY_DSN = "https://ae61dd884a8c4e78800817bcff385afc@sentry.sme.prefeitura.sp.gov.br/20";;
 }
