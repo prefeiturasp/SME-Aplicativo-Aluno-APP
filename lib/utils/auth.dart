db0134b1d9eb2b0cebf15a942cb574672659aba7
Diego Collares <diego.collares@gmail.com>
2020-08-18 19:12:12 -0300

    updated deletd user logout

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,31 +1,22 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:sme_app_aluno/screens/login/login.dart';
+import 'package:sme_app_aluno/services/user.service.dart';
 import 'package:sme_app_aluno/utils/storage.dart';
 
 class Auth {
-  static logout(BuildContext context) async {
-    FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  static logout(BuildContext context, int id) async {
+    // FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+    final UserService _userService = UserService();
 
-    var ids = new List<int>.generate(20, (i) => i + 1);
-    ids.forEach((element) {
-      print("logout remove Ids: $element");
-      _firebaseMessaging.unsubscribeFromTopic(element.toString());
-    });
+    // var ids = new List<int>.generate(20, (i) => i + 1);
+    // ids.forEach((element) {
+    //   print("logout remove Ids: $element");
+    //   _firebaseMessaging.unsubscribeFromTopic(element.toString());
+    // });
 
-    removeCurrentUser();
+    await _userService.delete(id);
     // prefs.clear();
     Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
-  }
-
-  static removeCurrentUser() {
-    final Storage _storage = Storage();
-    _storage.removeKey('current_user_id');
-    _storage.removeKey('current_name');
-    _storage.removeKey('current_cpf');
-    _storage.removeKey('current_email');
-    _storage.removeKey('token');
-    _storage.removeKey('current_password');
-    _storage.removeKey('dispositivo_id');
   }
 }
