9dc18b376e4cf41da1ea06422b1120da988ef83b
Diego Collares <diego.collares@gmail.com>
2020-09-17 13:03:28 -0300

    created structure and services

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:sme_app_aluno/models/message/group.dart';
 import 'package:sme_app_aluno/screens/login/login.dart';
+import 'package:sme_app_aluno/services/group_messages.service.dart';
 import 'package:sme_app_aluno/services/user.service.dart';
 import 'package:sme_app_aluno/utils/navigator.dart';
 
 class Auth {
   static logout(BuildContext context, int userId) async {
-    // FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+    FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
     final UserService _userService = UserService();
+    final GroupMessageService _groupMessageService = GroupMessageService();
 
-    // var ids = new List<int>.generate(20, (i) => i + 1);
-    // ids.forEach((element) {
-    //   print("logout remove Ids: $element");
-    //   _firebaseMessaging.unsubscribeFromTopic(element.toString());
-    // });
+    List<Group> groups = await _groupMessageService.all();
+
+    groups.forEach((element) {
+      print("Grupo removido: ${element.toMap()}");
+      _firebaseMessaging.unsubscribeFromTopic(element.codigo);
+      _groupMessageService.delete(element.id);
+    });
 
     await _userService.delete(userId);
     // prefs.clear();
