95250d45d0492ea5fc1585906e2f833d3c7eefdd
Diego Collares <diego.collares@gmail.com>
2020-11-16 00:40:51 -0300

    Criado card com frquencial global

--- lib/screens/widgets/cards/simple_card.dart
+++ lib/screens/widgets/cards/simple_card.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class SimpleCard extends StatelessWidget {
   @override
@@ -24,16 +25,45 @@ class SimpleCard extends StatelessWidget {
         ],
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          AutoSizeText(
-            "Frequência global do estudante",
-            textAlign: TextAlign.left,
-            maxFontSize: 22,
-            minFontSize: 18,
-            maxLines: 2,
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
+          Expanded(
+            flex: 3,
+            child: CircularPercentIndicator(
+                radius: 67.0,
+                lineWidth: 6.0,
+                animation: true,
+                animationDuration: 3000,
+                percent: 0.68,
+                animateFromLastPercent: true,
+                center: AutoSizeText(
+                  "68%",
+                  maxFontSize: 20,
+                  minFontSize: 18,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                circularStrokeCap: CircularStrokeCap.round,
+                progressColor: Color(0xffF6461F),
+                backgroundColor: Color(0xffF1F0F5),
+                reverse: true),
+          ),
+          Expanded(
+            flex: 7,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: AutoSizeText(
+                "Frequência global do estudante",
+                textAlign: TextAlign.left,
+                maxFontSize: 22,
+                minFontSize: 18,
+                maxLines: 2,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
           )
         ],
