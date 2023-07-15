382d2ef7b3537df7c944b73d4fa97ce66768aa3b
Thiago Fernando <thiago.fernando@msn.com>
2023-07-15 17:24:37 -0400

    Correção AutenticacaoController

--- lib/controllers/autenticacao.controller.dart
+++ lib/controllers/autenticacao.controller.dart
@@ -1,31 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:sme_app_aluno/models/index.dart';
 import 'package:sme_app_aluno/repositories/authenticate_repository.dart';
 import 'package:sme_app_aluno/stores/usuario.store.dart';
 
 class AutenticacaoController {
   AuthenticateRepository repository;
+  AutenticacaoController({
+    required this.repository,
+  });
   final usuarioStore = GetIt.I.get<UsuarioStore>();
-
-  AutenticacaoController() {
-    repository = AuthenticateRepository();
-  }
 
   Future<UsuarioDataModel> authenticateUser(String cpf, String password) async {
     var data = await repository.loginUser(cpf, password);
 
     if (data == null) {
-      return new UsuarioDataModel(data: null, erros: null, ok: false);
+      return new UsuarioDataModel(data: UsuarioModel(), erros: [], ok: false);
     }
 
-    if (data.data != null) {
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString('eaUsuario', jsonEncode(data.data.toJson()));
-      await usuarioStore.carregarUsuario();
-    }
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('eaUsuario', jsonEncode(data.data.toJson()));
+    await usuarioStore.carregarUsuario();
 
     return data;
   }
