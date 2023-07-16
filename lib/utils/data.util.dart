3d96944532f8018850b3ef70403cd423d534777e
Thiago Fernando <thiago.fernando@msn.com>
2023-07-16 19:50:00 -0400

    correção null safety utils

--- lib/utils/data.util.dart
+++ lib/utils/data.util.dart
@@ -1,12 +1,9 @@
 class DataUtil {
   static DateTime converteParaDataLocal(String value) {
-    return value != null
-        ? (DateTime.tryParse(value) != null
-            ? (value.toString().length >= 30
-                ? DateTime.parse(
-                    value.toString().substring(0, value.toString().length - 6))
-                : DateTime.parse(value.toString()))
-            : null)
-        : null;
+    return (DateTime.tryParse(value) != null
+        ? (value.toString().length >= 30
+            ? DateTime.parse(value.toString().substring(0, value.toString().length - 6))
+            : DateTime.parse(value.toString()))
+        : DateTime.now());
   }
 }
