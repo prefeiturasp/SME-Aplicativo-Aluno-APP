fa4d3345cb4688b097b617953476a9b35476fa32
Thiago Fernando <thiago.fernando@msn.com>
2023-07-16 14:35:34 -0400

    BackgroundFetchController g.dart

--- lib/controllers/background_fetch/background_fetch.controller.g.dart
+++ lib/controllers/background_fetch/background_fetch.controller.g.dart
@@ -8,9 +8,9 @@ part of 'background_fetch.controller.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$BackgroundFetchController on _BackgroundFetchControllerBase, Store {
+mixin _$BackgroundFetchController on BackgroundFetchControllerBase, Store {
   late final _$responsibleHasStudentAtom = Atom(
-      name: '_BackgroundFetchControllerBase.responsibleHasStudent',
+      name: 'BackgroundFetchControllerBase.responsibleHasStudent',
       context: context);
 
   @override
@@ -28,7 +28,7 @@ mixin _$BackgroundFetchController on _BackgroundFetchControllerBase, Store {
   }
 
   late final _$initPlatformStateAsyncAction = AsyncAction(
-      '_BackgroundFetchControllerBase.initPlatformState',
+      'BackgroundFetchControllerBase.initPlatformState',
       context: context);
 
   @override
@@ -39,7 +39,7 @@ mixin _$BackgroundFetchController on _BackgroundFetchControllerBase, Store {
   }
 
   late final _$checkIfResponsibleHasStudentAsyncAction = AsyncAction(
-      '_BackgroundFetchControllerBase.checkIfResponsibleHasStudent',
+      'BackgroundFetchControllerBase.checkIfResponsibleHasStudent',
       context: context);
 
   @override
