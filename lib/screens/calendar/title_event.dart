41141aae2d17a22346de48244d607b40633751cc
Thiago Fernando <thiago.fernando@msn.com>
2023-08-01 10:52:48 -0300

    ajuste tela eventos

--- lib/screens/calendar/title_event.dart
+++ lib/screens/calendar/title_event.dart
@@ -1,12 +1,13 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:sme_app_aluno/utils/string_support.dart';
+import '../../utils/string_support.dart';
 
 class TitleEvent extends StatelessWidget {
   final String dayOfWeek;
   final String title;
 
-  TitleEvent({
+  const TitleEvent({
+    super.key,
     required this.dayOfWeek,
     required this.title,
   });
@@ -17,24 +18,25 @@ class TitleEvent extends StatelessWidget {
         Expanded(
           flex: 2,
           child: AutoSizeText(
-            "$dayOfWeek",
+            dayOfWeek,
             maxFontSize: 14,
             minFontSize: 12,
             maxLines: 1,
             style: TextStyle(
-                color: dayOfWeek == "Sab" || dayOfWeek == "Dom" ? Colors.red : Colors.black,
-                fontWeight: FontWeight.w500),
+              color: dayOfWeek == 'Sab' || dayOfWeek == 'Dom' ? Colors.red : Colors.black,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
         Expanded(
           flex: 8,
           child: AutoSizeText(
-            StringSupport.truncateEndString(title, 40),
+            StringSupport.truncateEndString(title, 26),
             textAlign: TextAlign.left,
             maxFontSize: 14,
             minFontSize: 12,
             maxLines: 2,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.black,
             ),
           ),
