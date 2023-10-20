a61874440aad2fdf9cfebcb5d790526f07d52f4e
Thiago Fernando <thiago.fernando@msn.com>
2023-10-20 10:56:39 -0300

    Atualização pacotes e useMaterial3 false

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,7 @@ class MyApp extends StatelessWidget {
         color: const Color(0xffEEC25E),
         theme: ThemeData(
           primaryColor: const Color(0xFFEEC25E),
+          useMaterial3: false,
         ),
         home: const FluxoInicialView(),
         builder: EasyLoading.init(),
